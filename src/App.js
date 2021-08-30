9fa4256eb659526579eb0d1fc415d98eac80c6cf
Nicolas <nicolas.veliz@usach.cl>
2021-08-30 18:32:37 -0400

    Se añade navbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <div className="content">
+        <h1>App Component de la wea</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="interno">
+                <h1>Start Bootstrap</h1>
+                <div className="link">
+                    <a href="/">Home</a>
+                    <a href="/">About</a>
+                    <a href="/">Services</a>
+                    <a href="/">Contact</a>
+                </div>
+            </div>
+        </nav>
+      );
+}
+ 
+export default Navbar ;
